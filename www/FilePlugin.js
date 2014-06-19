a5b965d5700bd8d4832420d772e562d67f6668c0
Jean-Baptiste Perrin <jbp1@orange.fr>
2014-06-19 22:23:41 +0100

    Update FilePlugin.js
    Start ios implementation

--- www/FilePlugin.js
+++ www/FilePlugin.js
@@ -1,69 +1,57 @@
 var FilePlugin = {
-    createFile: function(data, filepath, successCallback, errorCallback) {
- 		cordova.exec(
- 			successCallback,
- 			errorCallback,
- 			'FilePlugin',
- 			'writefile',
- 			[{
- 				"data" : data,
- 				"filepath" : filepath
- 			}]
- 		);
-    },
-    openFile: function(filepath, content_type, successCallback, errorCallback) {
- 		cordova.exec(
- 			successCallback,
- 			errorCallback,
- 			'FilePlugin',
- 			'openfile',
- 			[{
-				"content_type" : content_type,
- 				"filepath" : filepath
- 			}]
- 		);
-    },
-    deleteFile: function(filepath, successCallback, errorCallback) {
- 		cordova.exec(
- 			successCallback,
- 			errorCallback,
- 			'FilePlugin',
- 			'deletefile',
- 			[{
- 				"filepath" : filepath
- 			}]
- 		);
-    },
-    createDirectory: function(filepath, successCallback, errorCallback) {
- 		cordova.exec(
- 			successCallback,
- 			errorCallback,
- 			'FilePlugin',
- 			'createdirectory',
- 			[{
- 				"filepath" : filepath
- 			}]
- 		);
-    },
-    deleteDirectory: function(filepath, successCallback, errorCallback) {
- 		cordova.exec(
- 			successCallback,
- 			errorCallback,
- 			'FilePlugin',
- 			'deletedirectory',
- 			[{
- 				"filepath" : filepath
- 			}]
- 		);
-    },
-    externalDirectory: function(successCallback, errorCallback) {
- 		cordova.exec(
- 			successCallback,
- 			errorCallback,
- 			'FilePlugin',
- 			'externaldirectory',
- 			[{}]
- 		);
-    }
+createFile: function(data, filepath, successCallback, errorCallback) {
+    cordova.exec(
+                 successCallback,
+                 errorCallback,
+                 'FilePlugin',
+                 'writefile',
+                 [data, filepath]
+                 );
+},
+openFile: function(filepath, content_type, successCallback, errorCallback) {
+    cordova.exec(
+                 successCallback,
+                 errorCallback,
+                 'FilePlugin',
+                 'openfile',
+                 [content_type, filepath]
+                 );
+},
+deleteFile: function(filepath, successCallback, errorCallback) {
+    cordova.exec(
+                 successCallback,
+                 errorCallback,
+                 'FilePlugin',
+                 'deletefile',
+                 [filepath]
+                 );
+},
+createDirectory: function(filepath, successCallback, errorCallback) {
+    cordova.exec(
+                 successCallback,
+                 errorCallback,
+                 'FilePlugin',
+                 'createdirectory',
+                 [filepath]
+                 );
+},
+deleteDirectory: function(filepath, successCallback, errorCallback) {
+    cordova.exec(
+                 successCallback,
+                 errorCallback,
+                 'FilePlugin',
+                 'deletedirectory',
+                 [filepath]
+                 );
+},
+externalDirectory: function(successCallback, errorCallback) {
+    cordova.exec(
+                 successCallback,
+                 errorCallback,
+                 'FilePlugin',
+                 'externaldirectory',
+                 []
+                 );
+}
 }
 module.exports = FilePlugin;
